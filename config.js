89f04ce7338f265c30308b6cc13bda4568b7e521
plehovlev2011-glitch <plehovlev2011@gmail.com>
2025-11-16 22:24:45 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+// Конфигурация GitHub API
+const GITHUB_CONFIG = {
+    // ЗАМЕНИТЕ НА ВАШИ ДАННЫЕ
+    owner: 'ВАШ_USERNAME_GITHUB',
+    repo: 'ВАШ_РЕПОЗИТОРИЙ',
+    token: 'ВАШ_GITHUB_TOKEN',
+    dataFile: 'bonus_data.json'
+};
+
+// Ключ шифрования (должен совпадать в клиентской и админ частях)
+const ENCRYPTION_KEY = 'pepper_bonus_secret_key_2024';
